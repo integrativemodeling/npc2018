4f0452c5a718ef014ecd7dc5c0c113eb0b91ce56
Ben Webb <ben@salilab.org>
2018-02-13 15:59:54 -0800

    Add Modeller scripts to build the complete complex

---
+++ data_nic96/MODELLER/combined/Build-combined.py
@@ -0,0 +1,27 @@
+from modeller import *
+from modeller.automodel import *
+from modeller.scripts import complete_pdb
+
+log.verbose()
+env = environ()
+a = alignment(env, file='alignment.ali')
+a.write(file='alignment.pap', alignment_format='PAP')
+
+class MyModel(automodel):
+    def special_patches(self, aln):
+        # Chain A: Nic96; Chain B: Nsp1 (3 segments)
+        # Chain C: Nup49; Chain D: Nup57
+        self.rename_segments(segment_ids=['A', 'B', 'B', 'B', 'C', 'D'],
+                             renumber_residues=[20, 637, 742, 788, 270, 287])
+
+a = MyModel(env,
+            alnfile='alignment.ali',
+            knowns=('5cws_F', '5cws_C1', '5cws_C2', '5cws_C3', '5cws_D',
+                    '5cws_E'),  
+            sequence='Nic96_complex',
+            assess_methods=(assess.DOPE, assess.GA341))
+
+a.starting_model = 1
+a.ending_model = 1
+
+a.make()
